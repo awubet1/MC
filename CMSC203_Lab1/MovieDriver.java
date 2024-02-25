5291dd051d1a12b3b1774d868397bb63b41c1780
awubet1 <102681678+awubet1@users.noreply.github.com>
2024-02-25 01:38:59 -0500

    Add files via upload

---
+++ CMSC203_Lab1/MovieDriver.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class MovieDriver {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean continueInput = true;
+        while (continueInput) {
+            Movie movie = new Movie();
+
+            System.out.println("Enter the title of the movie:");
+            String title = scanner.nextLine();
+            movie.setTitle(title);
+
+            System.out.println("Enter the rating of the movie:");
+            String rating = scanner.nextLine();
+            movie.setRating(rating);
+
+            System.out.println("Enter the number of tickets sold:");
+            int ticketsSold = scanner.nextInt();
+            movie.setTicketsSold(ticketsSold);
+
+            scanner.nextLine(); // Consume newline character
+
+            System.out.println(movie.toString());
+
+            System.out.println("Do you want to enter another movie? (yes/no)");
+            String userInput = scanner.nextLine();
+            if (!userInput.equalsIgnoreCase("yes")) {
+                continueInput = false;
+            }
+        }
+
+        scanner.close();
+    }
+
+    static class Movie {
+        private String title;
+        private String rating;
+        private int ticketsSold;
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void setRating(String rating) {
+            this.rating = rating;
+        }
+
+        public void setTicketsSold(int ticketsSold) {
+            this.ticketsSold = ticketsSold;
+        }
+
+        @Override
+        public String toString() {
+            return "Movie [title=" + title + ", rating=" + rating + ", ticketsSold=" + ticketsSold + "]";
+        }
+    }
+}
